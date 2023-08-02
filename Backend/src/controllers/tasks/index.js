eb096c7811227fceb278d2e3eb777b2bf9972172
IsaacOrtga <ortga.isaac@gmail.com>
2023-08-02 20:22:39 +0200

    t2 done

--- Backend/src/controllers/tasks/index.js
+++ Backend/src/controllers/tasks/index.js
@@ -16,4 +16,5 @@ module.exports = {
   getTasksById,
   deleteTask,
   getTaskByProfileId,
+
 }
--- Frontend/src/screens/control-panel/Catalog/TaskGeneral/TaskGeneralListScreen/TaskGeneralListScreen.js
+++ Frontend/src/screens/control-panel/Catalog/TaskGeneral/TaskGeneralListScreen/TaskGeneralListScreen.js
@@ -26,7 +26,7 @@ import styles from './styles/taskGeneralListScreenStyles'
 
 const useStyles = makeStyles(styles)
 
-const TaskGeneralListScreen = () => {
+const TaskGeneralListScreen = (errorTaksList) => {
   const ExcelFile = ReactExport.ExcelFile
   const dispatch = useDispatch()
   const classes = useStyles()
